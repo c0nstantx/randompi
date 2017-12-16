862a8b8e1304c7fd630be62ad06670cb692afe35
Konstantinos Christofilos <kostasxx@gmail.com>
2017-12-16 14:03:27 +0200

    List videos in given path. Play/Stop using VLC

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"log"
+	"os/exec"
+	"randompi/app/services"
+
 	"github.com/revel/revel"
 )
 
@@ -9,5 +13,72 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
+	vl := services.VideoList("/home/kostasx/Downloads/test_videos")
+
+	return c.Render(vl)
+}
+
+func (c App) Random() revel.Result {
+	closePlayers()
+	vl := services.VideoList("/home/kostasx/Downloads/test_videos")
+	// command := "vlc"
+	for _, v := range vl {
+		cmd := exec.Command("vlc", "--playlist-enqueue", v.Path)
+		err := cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	return c.Render()
+}
+
+func (c App) Play() revel.Result {
+	closePlayers()
+	vl := services.VideoList("/home/kostasx/Downloads/test_videos")
+	videoHash := c.Params.Query.Get("v")
+	video := vl[videoHash]
+	cmd := exec.Command("vlc", "file://"+video.Path)
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c.Render(video)
+}
+
+func (c App) Stop() revel.Result {
+	closePlayers()
+	return c.Redirect(App.Index)
+}
+
+func (c App) Pause() revel.Result {
+	cmd := exec.Command("vlc", "vlc://pause")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	vl := services.VideoList("/home/kostasx/Downloads/test_videos")
+	videoHash := c.Params.Query.Get("v")
+	video := vl[videoHash]
+	return c.Render(video)
+}
+
+func (c App) Resume() revel.Result {
+	cmd := exec.Command("vlc", "vlc://pause")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	vl := services.VideoList("/home/kostasx/Downloads/test_videos")
+	videoHash := c.Params.Query.Get("v")
+	video := vl[videoHash]
+	return c.Render(video)
+}
+
+func closePlayers() {
+	// cmd := exec.Command("vlc", "vlc://stop")
+	cmd := exec.Command("killall", "-9", "vlc")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ app/services/videolist.go
@@ -0,0 +1,64 @@
+package services
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/h2non/filetype.v1"
+)
+
+type videoFile struct {
+	Hash string
+	Name string
+	Path string
+}
+
+func VideoList(path string) map[string]videoFile {
+	return readFiles(path)
+}
+
+func readFiles(path string) map[string]videoFile {
+	// var fileList map[string]videoFile
+	fileList := make(map[string]videoFile)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fileName := file.Name()
+		filePath := filepath.Join(path, fileName)
+		if file.IsDir() {
+			folderFiles := readFiles(filePath)
+			for _, folderFile := range folderFiles {
+				fileList[folderFile.Hash] = folderFile
+				// fileList = append(fileList, folderFile)
+			}
+		} else {
+			f := buildVideoFile(fileName, filePath)
+			if len(f.Name) > 0 {
+				fileList[f.Hash] = f
+				// fileList = append(fileList, f)
+			}
+		}
+	}
+
+	return fileList
+}
+
+func buildVideoFile(name string, path string) videoFile {
+	file, _ := os.Open(path)
+	head := make([]byte, 261)
+	file.Read(head)
+	if filetype.IsVideo(head) {
+		hashData := []byte(path)
+		hashBytes := md5.Sum(hashData)
+		hash := fmt.Sprintf("%x", hashBytes)
+		return videoFile{hash, name, path}
+	}
+
+	return videoFile{}
+}
